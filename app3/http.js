1c0f7d7c96eb4d92115e8519b34d6fb38764ecb8
ahmedsayedtalib <ahmedsayed747@hotmail.com>
2024-11-11 16:46:32 +0200

    updates and added history command

--- app3/http.js
+++ app3/http.js
@@ -9,7 +9,9 @@ else if (req.url === '/about') {
 }
 else {
     res.end('<h1 style:"font-color:red">Oops!</h1><p>Why are you even here?<p> \
-        <a href = "/">Home page</a>')
+        <a href = "/">Home page</a> \
+        <input style="border-radius: 20px 20px 20px 20px; width: 100px; height: 50px; \
+         color: aqua;" type="text" value="Enter" placeholder="Enter name Here">Add This</input>')
 }
 })
 
