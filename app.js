4ed7b33e775fb87941eda76d18f2543071d963b5
ahmedsayedtalib <ahmedsayed747@hotmail.com>
2024-11-16 20:50:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require ('express')
 const {products} = require('./data')
 
+const app = express()
+
 app.get('/',(req,res)=>{
-   res.send("<h1>Welcome to asto<span style = 'color:green'>S</span>tore</h1><a href = '/api/products'>Products</a>")
+    res.status(200).send("<h1 style = 'color:skyblue'>welcome to asto<span style = 'color:green'>S</span>tore</h1>\
+       <a style ='green' href = '/api/products'>products page</a> ")
 })
 
 app.get('/api/products',(req,res)=>{
-    const newProduct = products.map((product)=>{ 
+    const newProduct = products.map((product)=>{
         const {id,name,image} = product
         return {id,name,image}
     })
-    res.json(newProduct)
+    res.status(200).json(newProduct)
 })
-
-
 
 app.get('/api/products/:productID',(req,res)=>{
     const {productID} = req.params
     const singleProduct = products.find((product)=> product.id === Number(productID))
-    if(!singleProduct) {
-        return res.status(404).send('No such Product')
+    if (!singleProduct) {
+        res.status(404).send('No product found')
+        console.log(req.params)
+        console.log(req)
     }
-    res.json(singleProduct)
-
-    console.log(req)
-    console.log(req.params)
+    else {
+        res.status(200).send(singleProduct)
+        console.log(req.params)
+    }
 })
 
 app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
     console.log(req.params)
-    res.send('Hello, Ahmed')
+    res.send('Hello, world!')
+})
+
+app.get('/api/v1/query',(req,res)=>{
+    const {search,limit} = req.query
+    let sortedProducts = [...products]
+    if (search) {
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts.slice(0,Number(limit))
+    }
+    res.status(200).json(sortedProducts)
+    console.log(req.query)
+    if (sortedProducts.length < 1) {
+        // res.status(200).send('No products  matched your search')
+        res.status(200).json({success:true})
+    }
 })
 
 
-app.get('/all',(req,res)=>{
-    res.json(products)
-})
-
-app.listen(5000,()=>{console.log("Server is listening on port 5000")})
+app.listen(5000,console.log("App started on port 5000")) 
